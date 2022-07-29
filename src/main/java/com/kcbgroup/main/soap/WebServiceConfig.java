1406354ce83524441123a99d5c828092a9390f69
KEN20960 <muchirinephat5@gmail.com>
2022-07-29 15:57:25 +0300

    I am too lazy to add a comment

--- src/main/java/com/kcbgroup/main/soap/WebServiceConfig.java
+++ src/main/java/com/kcbgroup/main/soap/WebServiceConfig.java
@@ -1,7 +1,33 @@
 package com.kcbgroup.main.soap;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
 /**
  * @ Author NMuchiri
  **/
+
+@EnableWs
+@Configuration
+@Slf4j
 public class WebServiceConfig {
+    /*                  GLOBAL VARIABLES                       */
+
+    @Bean
+    ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
+        MessageDispatcherServlet messageDispatcherServlet =
+                new MessageDispatcherServlet();
+        messageDispatcherServlet.setApplicationContext(context);
+        messageDispatcherServlet.setTransformWsdlLocations(true);
+
+        return new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
+    }
+
+
+
 }
