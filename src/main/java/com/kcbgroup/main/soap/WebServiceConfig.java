a857cd788ebe2799a254accd3f582334f55a3e1b
KEN20960 <muchirinephat5@gmail.com>
2022-07-29 17:13:48 +0300

    I am too lazy to add a comment

--- src/main/java/com/kcbgroup/main/soap/WebServiceConfig.java
+++ src/main/java/com/kcbgroup/main/soap/WebServiceConfig.java
@@ -5,8 +5,12 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 /**
  * @ Author NMuchiri
@@ -19,7 +23,7 @@ public class WebServiceConfig {
     /*                  GLOBAL VARIABLES                       */
 
     @Bean
-    ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext context) {
         MessageDispatcherServlet messageDispatcherServlet =
                 new MessageDispatcherServlet();
         messageDispatcherServlet.setApplicationContext(context);
@@ -28,6 +32,23 @@ public class WebServiceConfig {
         return new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
     }
 
+    @Bean(name = "courses")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema coursesSchema) {
+        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+        definition.setPortTypeName("CoursePort");
+        definition.setTargetNamespace("http://in28minutes.com/courses");
+        definition.setLocationUri("/ws");
+        definition.setSchema(coursesSchema);
+
+
+        return definition;
+    }
+
+    // ws/courses.wsdl
+    @Bean
+    public XsdSchema coursesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));
+    }
 
 
 }
