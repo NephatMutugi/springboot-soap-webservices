8772fbb6241496e2e56e4e0a3dbf28fa85b4d58a
KEN20960 <muchirinephat5@gmail.com>
2022-07-29 15:15:51 +0300

    Create Soap endpoints

---
+++ src/main/java/com/kcbgroup/main/soap/CourseDetailsEndpoint.java
@@ -0,0 +1,65 @@
+package com.kcbgroup.main.soap;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.kcbgroup.main.entities.CourseDetails;
+import com.kcbgroup.main.entities.GetCourseDetailsRequest;
+import com.kcbgroup.main.entities.GetCourseDetailsResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+/**
+ * @ Author NMuchiri
+ **/
+@Endpoint
+@Slf4j
+public class CourseDetailsEndpoint {
+
+    /* Set payload root by giving the namespace and element name defined in the xsd*/
+    @PayloadRoot(namespace = "http://in28minutes.com/courses", localPart = "GetCourseDetailsRequest")
+    @ResponsePayload
+    public GetCourseDetailsResponse
+    processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+        GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+        CourseDetails courseDetails = new CourseDetails();
+
+        // Request id
+        log.info("ID -> {}", request.getId());
+
+        courseDetails.setId(request.getId());
+        courseDetails.setName("Computer Science");
+        courseDetails.setDescription("This is the only course you will need");
+
+
+        /*  Object to json converter */
+        //Course Details
+        try {
+            ObjectWriter course = new ObjectMapper().writer().withDefaultPrettyPrinter();
+            String logCourse = course.writeValueAsString(courseDetails);
+            log.info("Course Details: {}", logCourse);
+        } catch (JsonProcessingException e) {
+            String errorMessage = e.getMessage();
+            log.error("Request Error: {}", errorMessage);
+        }
+
+
+        /*  Object to json converter */
+        //Expected response
+        try {
+            ObjectWriter resp = new ObjectMapper().writer().withDefaultPrettyPrinter();
+            String logResp = resp.writeValueAsString(response);
+            log.info("Response: {}", response);
+
+        } catch (JsonProcessingException e) {
+            String errorMessage = e.getMessage();
+            log.error("Response Error: {}", errorMessage);
+        }
+
+        return response;
+    }
+}
